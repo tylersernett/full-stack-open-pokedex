a0e5556a41e5d096981d64b374ca04f2d28399c0
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-21 13:17:45 -0400

    Add for individual pokemon pages

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('individual pokemon page can be opened from front page', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('squirtle').click()
+    cy.contains('hp')
+    cy.contains('torrent')
+  })
 })
